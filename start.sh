60261e9bdf2eb8cb882e63ab8516a290bd3f5e42
SCSeachBot <142865268+SCSeachBot@users.noreply.github.com>
2023-08-22 14:20:55 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone SCBots/Advance-Auto-Filter /Advance-Auto-Filter
+  git clone https://GitHub.com/scseachbot/Advance-Auto-Filter- /Advance-Auto-Filter-
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Advance-Auto-Filter
+  git clone $UPSTREAM_REPO /Advance-Auto-Filter-
 fi
-cd /Advance-Auto-Filter
+cd /Advance-Auto-Filter-
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
