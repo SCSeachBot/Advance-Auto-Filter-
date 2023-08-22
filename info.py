a4242148ec568be069ead3efd89cd89b51dc5bb6
SCSeachBot <142865268+SCSeachBot@users.noreply.github.com>
2023-08-22 18:22:54 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,7 +15,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_Bot')
 API_ID = int(environ.get('API_ID', '27958046'))
 API_HASH = environ.get('API_HASH', '19e4071c712aebcad001023f4fe3959f')
-BOT_TOKEN = environ.get('BOT_TOKEN', '6256823825:AAH7h6tAwti5puLmn9SzZn9cnY6a50mYmIE')
+BOT_TOKEN = environ.get('BOT_TOKEN',6256823825:AAH7h6tAwti5puLmn9SzZn9cnY6a50mYmIE ')
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
